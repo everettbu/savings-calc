3dbb1f50c6e6acd087491e2e08f8668edf7ac71d
Everett Butler <everettbutler@rocketmail.com>
2024-06-03 15:28:31 -0700

    Setup issues fixed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Vite and React!</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
-
 export default App;
