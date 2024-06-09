738bd5c37eeade8d2e29fb1ac445cb85d3fcaa7e
KaranGoel1 <kgoel24@cmc.edu>
2024-06-08 22:59:57 -0700

    created contact form

---
+++ savings-calculator/src/components/ContactForm.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const ContactForm = ({ onSubmit, inputClass, labelClass, errorClass, formClass }) => {
+  const validationSchema = Yup.object({
+    inquiryType: Yup.string().required('Required'),
+    name: Yup.number().required('Required'),
+    email: Yup.number().required('Required'),
+  });
+
+  return (
+    <Formik
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        onSubmit(values);
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting, values }) => (
+        <Form id="contact-form" className={formClass}>
+          <div className="mb-4">
+            <label htmlFor="inquiryType" className={labelClass}>Inquiry Category</label>
+            <Field as="select" name="inquiryType" id="inquiryType" className={inputClass}>
+              <option value="" label="Select inquiry category" />
+              <option value="Technical Questions" label="Technical Questions" />
+              <option value="Consumer Loans" label="Consumer Loans" />
+              <option value="General Information" label="General Information" />
+              <option value="Employment" label="Employment" />
+              <option value="Information Sharing Opt-Out" label="Information Sharing Opt-Out" />
+              <option value="Real Estate Loans" label="Real Estate Loans" />
+            </Field>
+            <ErrorMessage name="inquiryType" component="div" className={errorClass} />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className={labelClass}>Name</label>
+            <Field type="text" name="name" id="name" className={`${inputClass} no-spinner`} />
+            <ErrorMessage name="name" component="div" className={errorClass} />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className={labelClass}>Email</label>
+            <Field type="text" name="email" id="email" className={`${inputClass} no-spinner`} />
+            <ErrorMessage name="email" component="div" className={errorClass} />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="streetAddress" className={labelClass}>Street Address</label>
+            <Field type="text" name="streetAddress" id="streetAddress" className={`${inputClass} no-spinner`} />
+          </div>
+          <div className="mb-4 flex flex-col">
+                <div className="flex flex-row">
+                    <label htmlFor="city" className={`${labelClass} mr-20`}>City</label>
+                    <label htmlFor="state" className={`${labelClass} ml-12`}>State</label>
+                    <label htmlFor="zip" className={`${labelClass} ml-10`}>ZIP</label>
+                </div>
+                <div className='flex flex-row'>
+                    <Field type="text" name="city" id="city" className={`${inputClass} no-spinner mr-6`} style={{width: '130px'}}/>
+                    <Field type="text" name="state" id="state" className={`${inputClass} no-spinner mr-8`} style={{width: '40px'}} maxLength="2"/>
+                    <Field type="text" name="zip" id="zip" className={`${inputClass} no-spinner`} style={{width: '60px'}} maxLength="5"/>
+                </div>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="comment" className={labelClass}>Comment</label>
+            <textarea type="text" name="comment" id="comment" className={`${inputClass} no-spinner`} rows="5"/>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ContactForm;
--- savings-calculator/src/pages/Contact.jsx
+++ savings-calculator/src/pages/Contact.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
     return(
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col items-center">
             <header className="bg-primary text-white text-center py-1 w-full">
                 <h1 className="text-2xl font-bold">Contact First City</h1>
             </header>
+            <div className="border-4 border-secondary rounded-sm flex justify-center mt-10">
+                Call: (213) 482-3477 (if from 213 or adjacent area codes) or (800) 944-2200
+            </div>
             <main className="flex-col flex-1">
-                <div className="justify-center p-10">
-                    <div className="flex justify-center">
-                    <a href="https://www.firstcitycu.org/contact/email/" target="_blank" rel="noopener noreferrer">
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-300 mb-4 justify-center">
-                            Contact via Email
-                        </button>
-                    </a>
-                    </div>
-                    <div className='flex justify-center p-5'>
-                        OR
-                    </div>
-                    <div className='flex justify-center'>
-                        Call: (213) 482-3477 or (800) 944-2200
-                    </div>
+            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+                    <ContactForm
+                    formClass="space-y-6"
+                    inputClass="mt-1 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full"
+                    labelClass="block text-sm font-medium text-gray-700"
+                    errorClass="text-red-500 text-sm mt-1"
+                    />
+                    <button
+                    className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300 mt-4"
+                >
+                    Send
+                </button>
                 </div>
+                
             </main>
         </div>    
     )
