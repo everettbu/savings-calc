81b7602a6392b51e42c5c7cfe40b0bf8c9890c7b
Everett Butler <everettbutler@rocketmail.com>
2024-06-11 11:15:35 -0700

    UI changes 1

--- savings-calculator/src/components/DepositCard.jsx
+++ savings-calculator/src/components/DepositCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const DepositCard = ({ deposit, index, onEdit }) => {
   return (
     <div
-      className="bg-white p-6 rounded-lg shadow-lg border-l-4 border-gray-400 mb-4 cursor-pointer hover:shadow-xl transition-shadow duration-300 group"
+      className="bg-white p-6 shadow rounded-lg border-l-4 border-slate-400 mb-4 cursor-pointer hover:shadow-md transition-shadow duration-300 group"
       onClick={() => onEdit(index)}
     >
       <h3 className="text-xl font-semibold">{deposit.bank} Deposit Account</h3>
       <p>Balance: {deposit.balance.toLocaleString(undefined, {currency:'USD', style:'currency', currencyDisplay:'narrowSymbol'})}</p>
       <p>APR: {deposit.annualYield}%</p>
       <div className="relative">
-        <div className="mt-2 bg-secondary text-white font-bold p-2 rounded opacity-75 group-hover:opacity-100 transition-opacity duration-300 border-2 border-secondary md:absolute md:right-0 md:top-0 md:transform md:-translate-y-11 md:mt-0 md:inline-block"
+        <div className="mt-2 bg-secondary-light text-white font-bold p-2 rounded group-hover:bg-secondary transition-opacity duration-300 md:absolute md:right-0 md:top-0 md:transform md:-translate-y-11 md:mt-0 md:inline-block"
              style={{ display: 'inline-block' }}>
             Savings: {typeof deposit.savings === 'number' ? deposit.savings.toLocaleString(undefined, {currency:'USD', style:'currency', currencyDisplay:'narrowSymbol'}) : 'N/A'}
         </div>
--- savings-calculator/tailwind.config.js
+++ savings-calculator/tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
       colors: {
         primary: '#001e60', 
         'primary-dark': '#00133e',
+        'primary-light': '#94abdf', //95b2f1
         secondary: '#509e2f',
         'secondary-dark': '#3b7a23',
+        'secondary-light': '#5fbc38',
         tertiary: '#DE7C00',
-        'tertiary-dark': '#C06A00', // Slightly lighter dark shade for tertiary
+        'tertiary-dark': '#ca7100', 
       },
     },
   },
